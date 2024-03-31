0bae4c96367df021134809bf247e401c9331b68e
RasmusTorp <s214597@dtu.dk>
2024-03-31 22:12:43 +0200

    path to data

--- hpc_data_path_test.py
+++ hpc_data_path_test.py
@@ -2,4 +2,4 @@
 import os
 
 print("current working directory: ", os.getcwd())
-print("path two steps back from current working directory: ", os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir)))
+print("path two steps back from current working directory: ", os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir, "data", "data","11t51center")))
