ee28968e74a2b9eff0fb22025ccc024418334482
RasmusTorp <s214597@dtu.dk>
2024-05-15 00:05:47 +0200

    validation loader

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ from omegaconf import OmegaConf
 import hydra
 import wandb
 import os
-from dataLoad import get_dataloaders
+from dataLoad import get_dataloaders, data_load_tensors
 from UNET_2D import UNet2D
 import torch
 from plotting_functions.plot_all import plot_all
-from plotting_functions.plot_trough_network import plot_trough_network
 from evaluation import calculate_iou, calculate_pixel_accuracy
+from segmentSlice import segment_slice
 
 @hydra.main(config_name="config_test.yaml", config_path="./", version_base="1.3")
 def main(config):
@@ -17,9 +17,11 @@ def main(config):
     
     else: 
         folder_path = "data/11t51center"
+    
+    
         
-        
-    train_loader, test_loader = get_dataloaders(batch_size=config.hyper.batch_size, train_size=config.data.train_size, 
+    train_loader, test_loader, val_loader = get_dataloaders(batch_size=config.hyper.batch_size, train_size=config.data.train_size,
+                                                test_size=config.data.test_size, 
                                                 seed=config.constants.seed, sampling_height=config.data.sampling_height, 
                                                 sampling_width=config.data.sampling_width, 
                                                 in_memory=config.data.in_memory, 
@@ -38,22 +40,28 @@ def main(config):
                     n_depth=config.hyper.n_depth,
                     with_skip_connections=config.hyper.with_skip_connections)
     
-    model.load_model(f"saved_models/{config.miscellaneous.save_as}", map_location=torch.device('cpu'))
+    model.load_model(f"saved_models/{config.testing.model}", map_location=torch.device('cpu'))
     
-    if config.testing.test_on == "test":
+    # X, y = data_load_tensors(folder_path=folder_path)
+    
+    # y_i = y[config.testing.slice]
+    # segmentation = segment_slice(model, X[config.testing.slice], sampling_height=config.data.sampling_height, sampling_width=config.data.sampling_width, stride = 100)
+    
+    if config.testing.test_on == "test" and config.testing.plot:
         dataset = test_loader.dataset    
-        plot_all(model, dataset, config.testing.slice, "test")
+        plot_all(model, dataset, config.testing.slice, f"withflipping_test")
     
-    elif config.testing.test_on == "train":
+    elif config.testing.test_on == "train" and config.testing.plot:
         dataset = train_loader.dataset
-        plot_all(model, dataset, config.testing.slice, "train")
+        plot_all(model, dataset, config.testing.slice, f"withflipping_train")
         
     
     if config.testing.plot_trough_network:
+        dataset = test_loader.dataset  
         x, y  = dataset[config.testing.slice]
         x = x.unsqueeze(0)
         model.eval()
-        model.plot_trough_network(x, save_as="trough_network")
+        model.plot_trough_network(x, save_as=f"trough_network_withflipping_{config.testing.slice}")
     
     
     if config.testing.evaluate:
