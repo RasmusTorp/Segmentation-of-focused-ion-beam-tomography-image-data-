852d5a220a5ccaf8bcb147d7594d7d96a576264e
RasmusTorp <s214597@dtu.dk>
2024-05-23 13:48:17 +0200

    fix small error

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def main(config):
                     lr=config.hyper.lr, patience=config.hyper.patience, track = config.wandb.track, save_as=config.miscellaneous.save_as)
     
     pixel_accuracy, mean_iou = model.evaluate(test_loader)
-    wandb.track({"pixel_accuracy_test": pixel_accuracy, "mean_iou_test": mean_iou})
+    wandb.log({"pixel_accuracy_test": pixel_accuracy, "mean_iou_test": mean_iou})
     print("done")
     
 if __name__ == "__main__":
